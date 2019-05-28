ac3f037e6431d1b300bbc683e60cd3114092b1ca
alfathony <email.alfathony@gmail.com>
2019-05-28 21:19:36 +0700

    updated hero page and link sosmed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar/>
+        <Hero/>
       </div>
     );
   }
