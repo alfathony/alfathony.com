9e1c72e006d3717a94814cea40cb6436061f7336
Alfathony Mac Pro M3 <email.alfathony@gmail.com>
2026-04-19 23:56:37 +0700

    chore: add .gitignore and remove untracked build files from index

--- .nuxt/types/supabase.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '#supabase/server' {
-  const serverSupabaseClient: typeof import('/Users/alfathony/Documents/Frontend/alfathony.com/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseClient
-  const serverSupabaseServiceRole: typeof import('/Users/alfathony/Documents/Frontend/alfathony.com/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseServiceRole
-  const serverSupabaseUser: typeof import('/Users/alfathony/Documents/Frontend/alfathony.com/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseUser
-  const serverSupabaseSession: typeof import('/Users/alfathony/Documents/Frontend/alfathony.com/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseSession
-}
